8bb5d867da6f202f7698f3681c08f0d1b5cb4d27
Ankit Bhawnani <bhawnaniankit@gmail.com>
2024-06-08 23:10:42 +0530

    recoil added

---
+++ apps/user-app/app/layout.tsx
@@ -0,0 +1,17 @@
+
+import "./globals.css";
+import { Providers } from "../providers";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <Providers>
+        <body>{children}</body>
+      </Providers>
+    </html>
+  );
+}
